417d13f7463e5efaf12a439aece4b6f191e79686
Meghna0907 <singhalmeghna07@gmail.com>
2023-09-13 01:27:17 +0530

    Implement Visualization

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ new Vue({
             loading: false,
             wordCount: 0,
             charCount: 0,
+            words: [],
+            showChartHeader: false,
+            chart: null,
         };
     },
     methods:{
@@ -18,11 +21,12 @@ new Vue({
             reader.onload=()=>{
                 this.image=reader.result;
             };
-            this.charCount=0;
+            this.showChartHeader=false;
         },
         extractText(){
             debugger;
             this.loading=true;
+            this.showChartHeader=false;
             Tesseract.recognize(
                 this.image,
                 'eng',
@@ -37,11 +41,49 @@ new Vue({
         },
         calculateWordCount(){
             debugger;
-            const words = this.extractedText.toLowerCase().split(/[ ,\s\n.]+/).filter(Boolean);; // Split by whitespace /\s+/     /[ ,]+/
-            this.wordCount = words.length;
-            words.forEach(element => {
+            this.wordCount=0;
+            this.charCount=0;
+            this.showChartHeader=false;
+            this.words = this.extractedText.toLowerCase().split(/[ ,\s\n.]+/).filter(Boolean);; // Split by whitespace /\s+/     /[ ,]+/
+            this.wordCount = this.words.length;
+            this.words.forEach(element => {
                 this.charCount += element.length;
             });
-        }
+        },
+        createChart(){
+            debugger;
+            this.showChartHeader=true;
+            const wordCountDict={};
+            for(const word of this.words){
+                if(wordCountDict[word]){
+                    wordCountDict[word]++;
+                }
+                else{
+                    wordCountDict[word] = 1;
+                }
+            }
+            const canvas = document.getElementById("myChart");
+            let labels = Object.keys(wordCountDict);
+            let counts = Object.values(wordCountDict);
+            // Ensure the canvas is cleared before creating a new chart
+            if (this.chart) {
+                this.chart.destroy();
+            }
+            const ctx= canvas.getContext("2d");
+            const chartData ={
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Word Count",
+                        data: counts,
+                        borderwidth: 1,
+                    }
+                ],
+            };
+            this.chart =new Chart(ctx,{
+                type:"bar",
+                data: chartData,
+            });
+        },
     },
 });
